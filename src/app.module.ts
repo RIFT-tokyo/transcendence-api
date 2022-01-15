d4339985526be9157a5cc9491bbfb9140f080ae0
sasakiyudai <yudai14142@gmail.com>
2022-01-15 23:05:08 +0900

    setup for typeorm of user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [UsersModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'db',
+    port: 5432,
+    username: 'admin',
+    password: 'password',
+    database: 'transcendence',
+    entities: [User],
+    synchronize: true,
+  }),
+  UsersModule
+],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  exports: [TypeOrmModule],
+  providers: [UsersService],
+  controllers: [UsersController],
+})
 export class UsersModule {}
