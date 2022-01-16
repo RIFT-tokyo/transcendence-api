117c240b0180be18dd8e9a3996732d549b94fd48
sasakiyudai <yudai14142@gmail.com>
2022-01-16 17:49:40 +0900

    eslint for module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,19 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: "postgres",
-    host: "db",
-    port: 5432,
-    username: "admin",
-    password: "password",
-    database: "transcendence",
-    synchronize: false,
-    entities: ["dist/**/*.entity{.js,.ts}"],
-  }), UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'admin',
+      password: 'password',
+      database: 'transcendence',
+      synchronize: false,
+      entities: ['dist/**/*.entity{.js,.ts}'],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
