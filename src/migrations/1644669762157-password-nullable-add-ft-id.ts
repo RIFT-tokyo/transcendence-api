07f1066b7a9c6d04d513c5c1d022910b62880d5f
sasakiyudai <yudai14142@gmail.com>
2022-02-15 11:21:40 +0900

    linter

--- src/migrations/1644669762157-password-nullable-add-ft-id.ts
+++ src/migrations/1644669762157-password-nullable-add-ft-id.ts
@@ -1,18 +1,27 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class passwordNullableAddFtId1644669762157 implements MigrationInterface {
-    name = 'passwordNullableAddFtId1644669762157'
+export class passwordNullableAddFtId1644669762157
+  implements MigrationInterface
+{
+  name = 'passwordNullableAddFtId1644669762157';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "ft_id" integer`);
-        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_c8491296fbf8b61444cdc19bc6d" UNIQUE ("ft_id")`);
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "password" DROP NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" ADD "ft_id" integer`);
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD CONSTRAINT "UQ_c8491296fbf8b61444cdc19bc6d" UNIQUE ("ft_id")`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "password" DROP NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "password" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_c8491296fbf8b61444cdc19bc6d"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "ft_id"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "password" SET NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP CONSTRAINT "UQ_c8491296fbf8b61444cdc19bc6d"`,
+    );
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "ft_id"`);
+  }
 }
