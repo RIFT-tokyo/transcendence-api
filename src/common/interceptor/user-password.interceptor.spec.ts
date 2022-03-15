18ba09a807718d0ed2e52e136a1b741409c0ee67
nfukada <nfukada@student.42tokyo.jp>
2022-03-16 00:56:02 +0900

    chore(common/interceptor): add default test for UserPasswordInterceptor

---
+++ src/common/interceptor/user-password.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { UserPasswordInterceptor } from './user-password.interceptor';
+
+describe('UserPasswordInterceptor', () => {
+  it('should be defined', () => {
+    expect(new UserPasswordInterceptor()).toBeDefined();
+  });
+});
