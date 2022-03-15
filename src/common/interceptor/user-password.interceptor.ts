066179fa8fb3ed2d7e81c29a273830aed10c528d
nfukada <nfukada@student.42tokyo.jp>
2022-03-15 19:17:21 +0900

    feat(common/interceptor): add UserPasswordInterceptor

---
+++ src/common/interceptor/user-password.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+  data: T;
+}
+
+@Injectable()
+export class UserPasswordInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => {
+        if (data.password) {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          const { password, ...response } = data;
+          return response;
+        }
+        return data;
+      }),
+    );
+  }
+}
