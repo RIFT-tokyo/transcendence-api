4d9c9b64b733841fc7e9d1069bf2829461f0822b
tkomatsu <46949817+tkomatsu@users.noreply.github.com>
2022-04-05 20:14:00 +0900

    use try...catch...

--- src/seeds/user.seed.ts
+++ src/seeds/user.seed.ts
@@ -12,16 +12,18 @@ export class CreateUsers implements Seeder {
       const id1 = Math.floor(Math.random() * count) + 1;
       const id2 = Math.floor(Math.random() * count) + 1;
       if (!combinations.some((c) => c.id1 === id1 && c.id2 === id2)) {
-        await connection
-          .createQueryBuilder()
-          .insert()
-          .into('user_followers_user')
-          .values({
-            userId_1: id1,
-            userId_2: id2,
-          })
-          .execute();
-        combinations.push({ id1, id2 });
+        try {
+          await connection
+            .createQueryBuilder()
+            .insert()
+            .into('user_followers_user')
+            .values({
+              userId_1: id1,
+              userId_2: id2,
+            })
+            .execute();
+          combinations.push({ id1, id2 });
+        } catch (e) {}
       }
     }
   }
