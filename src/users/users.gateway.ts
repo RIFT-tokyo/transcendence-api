c6af454c3892a2ddcf92bd26faf44085612991dc
sasakiyudai <yudai14142@gmail.com>
2022-04-16 01:54:12 +0900

    remove not users event from usersgateway

--- src/users/users.gateway.ts
+++ src/users/users.gateway.ts
@@ -5,22 +5,18 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 import { UsersService } from './users.service';
 import { User } from '../generated/model/models';
 
-@WebSocketGateway({ cors: true })
-export class UsersGateway implements OnGatewayConnection, OnGatewayDisconnect {
+@WebSocketGateway({ cors: true, namespace: '/users' })
+export class UsersGateway {
   @Inject()
   usersService: UsersService;
 
   @WebSocketServer()
   server: Server;
-
-  // private userID;
 
   async updateUserStatus(status: User.StatusEnum, userID: number) {
     await this.usersService.updateUser(userID, { status: status });
@@ -28,23 +24,6 @@ export class UsersGateway implements OnGatewayConnection, OnGatewayDisconnect {
       status: status,
       userID,
     });
-  }
-
-  handleConnection(@ConnectedSocket() client: Socket) {
-    const userID = client.handshake.auth.userID;
-    console.log(`Client connected: ${userID}`);
-    this.updateUserStatus('online', userID);
-  }
-
-  handleDisconnect(@ConnectedSocket() client: Socket) {
-    const userID = client.handshake.auth.userID;
-    console.log(`Client DISconnected: ${userID}`);
-    this.updateUserStatus('offline', userID);
-  }
-
-  @SubscribeMessage('ping')
-  handlePing(@ConnectedSocket() client: Socket) {
-    client.emit('pong');
   }
 
   @SubscribeMessage('userStatus')
