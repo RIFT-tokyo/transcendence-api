1f17089ab74ad0adf17ddf226ec3cf1941a0c404
nfukada <nfukada@student.42tokyo.jp>
2022-04-23 00:27:57 +0900

    feat(generated): generate channel models and service from openapi.yaml

--- src/generated/api/api.ts
+++ src/generated/api/api.ts
@@ -1,9 +1,11 @@
 export * from './auth.service';
 import { AuthService } from './auth.service';
+export * from './channel.service';
+import { ChannelService } from './channel.service';
 export * from './follow.service';
 import { FollowService } from './follow.service';
 export * from './match.service';
 import { MatchService } from './match.service';
 export * from './user.service';
 import { UserService } from './user.service';
-export const APIS = [AuthService, FollowService, MatchService, UserService];
+export const APIS = [AuthService, ChannelService, FollowService, MatchService, UserService];
