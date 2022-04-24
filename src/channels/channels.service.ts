5eafb5f171ad4c3f50608c69b8eca57cc6992277
nfukada <nfukada@student.42tokyo.jp>
2022-04-24 23:34:06 +0900

    feat(channels): add channel resource

---
+++ src/channels/channels.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Channel } from '../entities/channel.entity';
+import { Repository } from 'typeorm';
+import { NewChannel } from 'src/generated';
+import { CreateChannelDTO, ResponseChannelDTO } from './channels.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class ChannelsService {
+  constructor(
+    @InjectRepository(Channel)
+    private readonly channelsRepository: Repository<Channel>,
+  ) {}
+
+  async findAll() {
+    return await this.channelsRepository.find();
+  }
+
+  async create(channel: NewChannel) {
+    if (channel.password) {
+      channel.password = await bcrypt.hash(
+        channel.password,
+        Number(process.env.HASH_SALT),
+      );
+    }
+    const result = await this.channelsRepository.save(
+      new CreateChannelDTO(channel),
+    );
+    return new ResponseChannelDTO(result);
+  }
+}
