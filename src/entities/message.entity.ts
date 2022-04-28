b8b46df226d731a1cd24a7b59a518c62c9dd8e64
nfukada <42476527+nafuka11@users.noreply.github.com>
2022-04-28 15:24:24 +0900

    feat(entities): add Message entity

---
+++ src/entities/message.entity.ts
@@ -0,0 +1,39 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import {
+  Check,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+
+@Entity()
+@Check(`"text" <> ''`)
+export class Message {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User)
+  user: User;
+
+  @Column()
+  @IsNotEmpty()
+  @IsString()
+  text: string;
+
+  @CreateDateColumn({ type: 'timestamp with time zone' })
+  @IsDate()
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp with time zone' })
+  @IsDate()
+  updated_at: Date;
+
+  @DeleteDateColumn({ type: 'timestamp with time zone' })
+  @IsDate()
+  deleted_at: Date | null;
+}
