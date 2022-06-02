e6d1df9ae3f4bcbd885ce40926f392d16357a408
nfukada <42476527+nafuka11@users.noreply.github.com>
2022-06-02 17:58:19 +0900

    feat(channels): add service file

--- src/channels/channel-user-permissions.service.ts
+++ src/channels/channel-user-permissions.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ChannelUserPermission } from 'src/entities/channel-user-permission.entity';
 import { Repository } from 'typeorm';
+import {
+  CreateChannelUserPermissionDTO,
+  UpdateChannelUserPermissionDTO,
+} from './channel-user-permissions.dto';
 
 @Injectable()
 export class ChannelUserPermissionsService {
@@ -10,4 +14,44 @@ export class ChannelUserPermissionsService {
     private readonly channelUsersRepository: Repository<ChannelUserPermission>,
   ) {}
 
+  async findByUserId(userId: number) {
+    return await this.channelUsersRepository.find({
+      where: { userId },
+      relations: ['user', 'channel', 'role'],
+    });
+  }
+
+  async findByChannelId(channelId: number) {
+    return await this.channelUsersRepository.find({
+      where: { channelId },
+      relations: ['user', 'channel', 'role'],
+    });
+  }
+
+  async create(channelUser: CreateChannelUserPermissionDTO) {
+    await this.channelUsersRepository.insert(channelUser);
+    return await this.channelUsersRepository.findOne(
+      { userId: channelUser.userId, channelId: channelUser.channelId },
+      { relations: ['user', 'channel', 'role'] },
+    );
+  }
+
+  async delete(channelId: number, userId: number) {
+    return await this.channelUsersRepository.softDelete({ channelId, userId });
+  }
+
+  async update(
+    channelId: number,
+    userId: number,
+    channelUser: UpdateChannelUserPermissionDTO,
+  ) {
+    await this.channelUsersRepository.update(
+      { channelId, userId },
+      channelUser,
+    );
+    return await this.channelUsersRepository.findOne(
+      { channelId, userId },
+      { relations: ['user', 'channel', 'role'] },
+    );
+  }
 }
