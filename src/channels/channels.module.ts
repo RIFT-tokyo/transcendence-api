8f2c1b6efeba0d50b7bda502853c8ac9a1dcd8c2
nfukada <42476527+nafuka11@users.noreply.github.com>
2022-06-02 18:05:34 +0900

    feat(channels): update module

--- src/channels/channels.module.ts
+++ src/channels/channels.module.ts
@@ -4,10 +4,11 @@ import { Channel } from '../entities/channel.entity';
 import { ChannelsController } from './channels.controller';
 import { ChannelsService } from './channels.service';
 import { ChannelUserPermissionsService } from './channel-user-permissions.service';
+import { ChannelUserPermission } from '../entities/channel-user-permission.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Channel])],
-  exports: [TypeOrmModule, ChannelsService],
+  imports: [TypeOrmModule.forFeature([Channel, ChannelUserPermission])],
+  exports: [TypeOrmModule, ChannelsService, ChannelUserPermissionsService],
   providers: [ChannelsService, ChannelUserPermissionsService],
   controllers: [ChannelsController],
 })
