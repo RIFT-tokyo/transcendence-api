f64d1406becceed30ce937a15fe3f77b1937670c
sasakiyudai <yudai14142@gmail.com>
2022-06-07 00:05:08 +0900

    import UsersModule, ChannelsGateway, Message, ChannelMessage

--- src/channels/channels.module.ts
+++ src/channels/channels.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChannelMessage } from 'src/entities/channel-message.entity';
+import { Message } from 'src/entities/message.entity';
+import { UsersModule } from 'src/users/users.module';
 import { Channel } from '../entities/channel.entity';
 import { ChannelsController } from './channels.controller';
+import { ChannelsGateway } from './channels.gateway';
 import { ChannelsService } from './channels.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Channel])],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forFeature([Channel, Message, ChannelMessage]),
+  ],
   exports: [TypeOrmModule, ChannelsService],
-  providers: [ChannelsService],
+  providers: [ChannelsService, ChannelsGateway],
   controllers: [ChannelsController],
 })
 export class ChannelsModule {}
