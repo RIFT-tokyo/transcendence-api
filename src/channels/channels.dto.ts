55b4bb34b40f7c1431f90c8807204b7c5046d086
nfukada <42476527+nafuka11@users.noreply.github.com>
2022-06-09 18:46:59 +0900

    feat(channel): update DTO classes

--- src/channels/channels.dto.ts
+++ src/channels/channels.dto.ts
@@ -1,20 +1,37 @@
-import { Channel } from '../generated/model/channel';
+import { ChannelUserPermission } from '../entities/channel-user-permission.entity';
 import { Channel as EntityChannel } from '../entities/channel.entity';
 import { ChannelMessage } from '../entities/channel-message.entity';
+import { Channel } from '../generated/model/channel';
+import { Role } from '../generated/model/role';
 
 export class ResponseChannelDTO implements Channel {
   id: number;
   name: string;
   is_protected: boolean;
+  role: Role;
   created_at?: string;
   updated_at?: string;
 
-  constructor(channel: EntityChannel) {
-    this.id = channel.id;
-    this.name = channel.name;
-    this.is_protected = !!channel.password;
-    this.created_at = channel.created_at.toISOString();
-    this.updated_at = channel.updated_at.toISOString();
+  constructor(channel: EntityChannel, userId: number);
+  constructor(channelUserPermission: ChannelUserPermission);
+  constructor(arg: EntityChannel | ChannelUserPermission, userId?: number) {
+    if (arg instanceof EntityChannel) {
+      const role = arg.users.find((v) => v.userId === userId)?.role ?? null;
+
+      this.id = arg.id;
+      this.name = arg.name;
+      this.is_protected = !!arg.password;
+      this.role = role;
+      this.created_at = arg.created_at.toISOString();
+      this.updated_at = arg.created_at.toISOString();
+    } else {
+      this.id = arg.channel.id;
+      this.name = arg.channel.name;
+      this.is_protected = !!arg.channel.password;
+      this.role = arg.role;
+      this.created_at = arg.channel.created_at.toISOString();
+      this.updated_at = arg.channel.updated_at.toISOString();
+    }
   }
 }
 
