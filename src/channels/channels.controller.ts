5b8eb201aa5e55a7b87f893bd14287d90b6debb1
nfukada <42476527+nafuka11@users.noreply.github.com>
2022-06-09 18:49:07 +0900

    feat: implement channels api

--- src/channels/channels.controller.ts
+++ src/channels/channels.controller.ts
@@ -1,26 +1,19 @@
 import {
   Body,
   Controller,
-  Delete,
   Get,
   HttpCode,
-  NotFoundException,
   Param,
   ParseIntPipe,
   Post,
-  Put,
   Session,
   UseGuards,
 } from '@nestjs/common';
-import { NewChannel } from 'src/generated';
-import { UserSession } from 'src/types/UserSession';
+import { NewChannel } from '../generated';
+import { UserSession } from '../types/UserSession';
 import { AuthenticatedGuard } from '../common/guards/authenticated.guard';
-import {
-  CreateChannelUserPermissionDTO,
-  ResponseChannelUserPermissionDTO,
-  UpdateChannelUserPermissionDTO,
-} from './channel-user-permissions.dto';
 import { ChannelUserPermissionsService } from './channel-user-permissions.service';
+import { ChannelUserDTO } from './channel-user.dto';
 import { ResponseChannelDTO } from './channels.dto';
 import { ChannelsService } from './channels.service';
 
@@ -28,14 +21,18 @@ import { ChannelsService } from './channels.service';
 export class ChannelsController {
   constructor(
     private readonly channelsService: ChannelsService,
-    private readonly channelUsersService: ChannelUserPermissionsService,
+    private readonly channelUserPermissionsService: ChannelUserPermissionsService,
   ) {}
 
   @UseGuards(AuthenticatedGuard)
   @Get()
-  async getChannels(): Promise<ResponseChannelDTO[]> {
+  async getChannels(
+    @Session() session: UserSession,
+  ): Promise<ResponseChannelDTO[]> {
     const channels = await this.channelsService.findAll();
-    return channels.map((channel) => new ResponseChannelDTO(channel));
+    return channels.map(
+      (channel) => new ResponseChannelDTO(channel, session.userId),
+    );
   }
 
   @UseGuards(AuthenticatedGuard)
@@ -45,88 +42,23 @@ export class ChannelsController {
     @Session() session: UserSession,
     @Body() channelData: NewChannel,
   ): Promise<ResponseChannelDTO> {
-    const channel = await this.channelsService.create(channelData);
-    if (!channel) {
-      throw new NotFoundException('Channel not found');
-    }
-    const channelUser: CreateChannelUserPermissionDTO = {
-      channelId: channel.id,
-      userId: session.userId,
-      is_ban: false,
-      role: { id: 1, name: 'owner' },
-    };
-    await this.channelUsersService.create(channelUser);
-    return new ResponseChannelDTO(channel);
-  }
-
-  @UseGuards(AuthenticatedGuard)
-  @Get('me')
-  async getMeChannels(
-    @Session() session: UserSession,
-  ): Promise<ResponseChannelUserPermissionDTO[]> {
-    const channelUsers = await this.channelUsersService.findByUserId(
-      session.userId,
-    );
-    return channelUsers.map(
-      (channelUser) => new ResponseChannelUserPermissionDTO(channelUser),
-    );
+    const channelUserPermission =
+      await this.channelUserPermissionsService.create(
+        channelData,
+        session.userId,
+      );
+    return new ResponseChannelDTO(channelUserPermission);
   }
 
   @UseGuards(AuthenticatedGuard)
   @Get(':id/users')
   async getChannelUsers(
     @Param('id', ParseIntPipe) id: number,
-  ): Promise<ResponseChannelUserPermissionDTO[]> {
-    const channelUsers = await this.channelUsersService.findByChannelId(id);
-    return channelUsers.map(
-      (channelUser) => new ResponseChannelUserPermissionDTO(channelUser),
+  ): Promise<ChannelUserDTO[]> {
+    const channelUserPermissions =
+      await this.channelUserPermissionsService.findByChannelId(id);
+    return channelUserPermissions.map(
+      (channelUser) => new ChannelUserDTO(channelUser),
     );
-  }
-
-  @UseGuards(AuthenticatedGuard)
-  @Post(':id')
-  async createChannelUser(
-    @Session() session: UserSession,
-    @Param('id', ParseIntPipe) id: number,
-  ): Promise<ResponseChannelUserPermissionDTO> {
-    const channelUserData: CreateChannelUserPermissionDTO = {
-      channelId: id,
-      userId: session.userId,
-      is_ban: false,
-      role: null,
-    };
-    const channelUser = await this.channelUsersService.create(channelUserData);
-    return new ResponseChannelUserPermissionDTO(channelUser);
-  }
-
-  @UseGuards(AuthenticatedGuard)
-  @Delete(':id')
-  @HttpCode(204)
-  async deleteChannelUser(
-    @Session() session: UserSession,
-    @Param('id', ParseIntPipe) id: number,
-  ): Promise<void> {
-    const ret = await this.channelUsersService.delete(id, session.userId);
-    if (ret.affected === 0) {
-      throw new NotFoundException('ChannelUser not found');
-    }
-  }
-
-  @UseGuards(AuthenticatedGuard)
-  @Put(':id/users/:userId')
-  async updateChannelUser(
-    @Param('id', ParseIntPipe) id: number,
-    @Param('userId', ParseIntPipe) userId: number,
-    @Body() channelUserData: UpdateChannelUserPermissionDTO,
-  ) {
-    const channelUser = await this.channelUsersService.update(
-      id,
-      userId,
-      channelUserData,
-    );
-    if (!channelUser) {
-      throw new NotFoundException('ChannelUser not found');
-    }
-    return new ResponseChannelUserPermissionDTO(channelUser);
   }
 }
