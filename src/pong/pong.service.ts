6f5577e72f88e76a249aac6db1de9d4f155b16ea
tkomatsu <46949817+tkomatsu@users.noreply.github.com>
2022-11-10 22:04:26 +0900

    wip
    Co-authored-by: syudai <yudai14142@gmail.com>
    Co-authored-by: nafuka <nafuka11@users.noreply.github.com>

--- src/pong/pong.service.ts
+++ src/pong/pong.service.ts
@@ -1,6 +1,32 @@
 import { Injectable } from '@nestjs/common';
 
+type Vector = [number, number, number];
+type BallState = {
+  position: Vector;
+  speed: number;
+  direction: Vector;
+}
+
+const X = 0;
+const Y = 1;
+const Z = 2;
+
 @Injectable()
 export class PongService {
+  calcBallPosition(
+    hostPosition: Vector,
+    guestPosition: Vector,
+    ballState: BallState,
+  ) {
+    ballState.position[X] += ballState.direction[X] * ballState.speed;
+    ballState.position[Z] += ballState.direction[Z] * ballState.speed;
 
+    if (ballState.direction[X] > ballState.speed * 2) {
+      ballState.direction[X] = ballState.speed * 2;
+    } else if (ballState.direction[X] < -ballState.speed * 2) {
+      ballState.direction[X] = -ballState.speed * 2;
+    }
+
+    if (ballState.direction[X] > 10)
+  }
 }
